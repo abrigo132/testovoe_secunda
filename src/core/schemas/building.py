84f8dd4d45e1e0c0615136753f89e4365b9bc0d3
abrigo132 <garipotter97@gmail.com>
2025-09-29 12:00:23 +0500

    add field validator

--- src/core/schemas/building.py
+++ src/core/schemas/building.py
@@ -1,5 +1,7 @@
-from pydantic import BaseModel, field_validator, ConfigDict
+from pydantic import BaseModel, field_validator, ConfigDict, model_validator, BeforeValidator
 from typing import List, Tuple
+from geoalchemy2.shape import to_shape
+
 
 from .base import OrganizationShort
 
@@ -15,16 +17,16 @@ class BuildingRead(BuildingBase):
     id: int
     coords: Tuple[float, float]
 
+    model_config = ConfigDict(from_attributes=True)
+
     @field_validator("coords", mode="before")
     @classmethod
-    def convert_geography(cls, v) -> Tuple[float, float]:
-        if hasattr(v, "x") and hasattr(v, "y"):
-            return float(v.x), float(v.y)
-        return v
+    def validate(cls, v):
+        geom = to_shape(v)
+        return geom.x, geom.y
 
 
-class BuildingWithOrganizationResponse(BaseModel):
-    building: BuildingRead
+class BuildingWithOrganizationResponse(BuildingRead):
     organizations: List["OrganizationShort"]
 
     model_config = ConfigDict(from_attributes=True)
